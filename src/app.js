e0ce0eac4b233888cf5d94ecc935352291f267dd
Yossi Zinger <yossi.zinger@gmail.com>
2023-09-07 01:02:11 +0300

    chore: add rate limiter

--- src/app.js
+++ src/app.js
@@ -1,12 +1,20 @@
 import express from 'express';
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
+import rateLimit from 'express-rate-limit';
+
+// Create a rate limiter
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100 // Limit each IP to 100 requests per windowMs
+});
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
 
+app.use(limiter)
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.get('/', (req, res) => {
