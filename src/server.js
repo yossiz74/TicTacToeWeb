a733bfc48ac1fbdce7de217bf9b11ee08785e14d
Yossi Zinger <yossi.zinger@gmail.com>
2023-09-07 00:55:43 +0300

    improve config.js

--- src/server.js
+++ src/server.js
@@ -1,6 +1,21 @@
+import config from './config.js';
+import winston from 'winston';
 import app from './app.js';
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.Console(),
+    //new winston.transports.File({ filename: 'combined.log' })
+  ]
+});
+
+const port = config.port;
+app.listen(port, (error) => {
+  if (error) {
+    logger.error('Error starting server:', error);
+  } else {
+    logger.info(`Server listening on port ${port}`);
+  }
 });
