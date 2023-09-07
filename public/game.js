7c83cb788757853218ffb75e0ac88ba6b17c85eb
Yossi Zinger <yossi.zinger@gmail.com>
2023-09-07 21:04:35 +0300

    feat: check winner

--- public/game.js
+++ public/game.js
@@ -1,2 +1,36 @@
 let board = [["", "", ""], ["", "", ""], ["", "", ""]];
 export { board };
+
+export function emptyBoard() {
+    board = [["", "", ""], ["", "", ""], ["", "", ""]];
+}
+export function makeMove(row, col, symbol) {
+    if (board[row][col] !== "") {
+        throw new Error("Invalid move");
+    }
+    board[row][col] = symbol;
+}
+export function checkWinner() {
+    // Check rows
+    for (let row = 0; row < 3; row++) {
+        if (board[row][0] !== "" &&
+            board[row][0] === board[row][1] &&
+            board[row][0] === board[row][2]) {
+            return board[row][0];
+        }
+    }
+    // Check columns
+    for (let col = 0; col < 3; col++) {
+        if (board[0][col] !== "" &&
+            board[0][col] === board[1][col] &&
+            board[0][col] === board[2][col]) {
+            return board[0][col];
+        }
+    }
+    // Check diagonals
+    if (board[0][0] !== "" &&
+        board[0][0] === board[1][1] &&
+        board[0][0] === board[2][2]) {
+            return board[0][0];
+        }
+}
--- tests/game.test.js
+++ tests/game.test.js
@@ -1,7 +1,36 @@
-import { board } from "../public/game.js";
+import { board, emptyBoard, makeMove, checkWinner, checkDraw } from "../public/game.js";
 
 describe("Game logic", () => {
+  beforeEach(() => {
+    emptyBoard()
+  });
   test("The board should be empty at the start", () => {
     expect(board).toEqual([["", "", ""], ["", "", ""], ["", "", ""]]);
+  });
+  test("A player can make a valid move by placing their symbol on an empty cell on the board", () => {
+    makeMove(0, 0, "X");
+    expect(board).toEqual([["X", "", ""], ["", "", ""], ["", "", ""]]);
+  });
+  test("A player cannot make an invalid move by placing their symbol on a cell that is already occupied", () => {
+    makeMove(0, 0, "X");
+    expect(() => makeMove(0, 0, "O")).toThrow();
+  });
+  test("The game correctly identifies a win condition when player has three of their symbols in a row", () => {
+    makeMove(0, 0, "X");
+    makeMove(0, 1, "X");
+    makeMove(0, 2, "X");
+    expect(checkWinner()).toBe("X");
+  });  
+  test("The game correctly identifies a win condition when player has three of their symbols in a column", () => {
+    makeMove(0, 0, "O");
+    makeMove(1, 0, "O");
+    makeMove(2, 0, "O");
+    expect(checkWinner()).toBe("O");
+  });  
+  test("The game correctly identifies a win condition when player has three of their symbols in a diagonal", () => {
+    makeMove(0, 0, "G");
+    makeMove(1, 1, "G");
+    makeMove(2, 2, "G");
+    expect(checkWinner()).toBe("G");
   });
 });
