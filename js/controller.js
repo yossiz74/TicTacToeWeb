d7e59141a1fc542512c4dcfcb9b7fb1dddadb82b
Yossi Zinger <yossi.zinger@gmail.com>
2023-09-08 23:35:01 +0300

    feat: add restart button (#12)

--- js/controller.js
+++ js/controller.js
@@ -1,13 +1,23 @@
-console.log('Hello from controller.js!');
+console.log('Hello from controller.js');
 
 import { initializeGame } from './game.js';
 import { handleCellClick } from './ui.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('DOM loaded!');
-    // Initialize the game when the DOM is ready
-    initializeGame();
+  console.log('DOM loaded!');
 
-    // Assign UI event handlers
-    window.handleCellClick = handleCellClick;
+  // Initialize the game when the DOM is ready
+  initializeGame();
+
+  // Assign UI event handlers
+  window.handleCellClick = handleCellClick;
+
+  // Get a reference to the restart button
+  const restartButton = document.getElementById('restart-button');
+
+  // Add a click event listener to the restart button
+  restartButton.addEventListener('click', () => {
+    // Reload the page to reset the game
+    location.reload();
+  });
 });
