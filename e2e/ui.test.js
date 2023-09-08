ec378635db2a651db6c07519eb6151a29fceee9d
Yossi Zinger <yossi.zinger@gmail.com>
2023-09-08 17:35:15 +0300

    refactor: reduce code duplication in ui tests

--- e2e/ui.test.js
+++ e2e/ui.test.js
@@ -19,18 +19,34 @@ describe('Mouse events', () => {
     await browser.close();
   });
 
-  test('When a user clicks on an empty cell, an X is written in the game board', async () => {
-    // Use Puppeteer's API to simulate a mouse click event
+  const winTheGame = async () => {
     await page.click('#cell-0-0');
-    // Use Jest's expect function to assert that the expected behavior occurred
+    await page.click('#cell-1-0');
+    await page.click('#cell-0-1');
+    await page.click('#cell-1-1');
+    await page.click('#cell-0-2');
+  };
+  
+  const drawTheGame = async () => {
+    await page.click('#cell-0-0');
+    await page.click('#cell-0-1');
+    await page.click('#cell-0-2');
+    await page.click('#cell-1-0');
+    await page.click('#cell-1-2');
+    await page.click('#cell-1-1');
+    await page.click('#cell-2-0');
+    await page.click('#cell-2-2');
+    await page.click('#cell-2-1');
+  };
+  
+
+  test('When a user clicks on an empty cell, an X is written in the game board', async () => {
+    await page.click('#cell-0-0');
     expect(await page.$eval('#cell-0-0', el => el.textContent)).toBe('X');
   });
   test('When a second user clicks on an empty cell, an O is written in the game board', async () => {
-    // Use Puppeteer's API to simulate a mouse click event
     await page.click('#cell-1-1');
     await page.click('#cell-0-0');
-    // Use Jest's expect function to assert that the expected behavior occurred
-    expect(await page.$eval('#cell-1-1', el => el.textContent)).toBe('X');
     expect(await page.$eval('#cell-0-0', el => el.textContent)).toBe('O');
   });
   test('When a user clicks on a cell that is already occupied, an error message is displayed', async () => {
@@ -47,56 +63,28 @@ describe('Mouse events', () => {
     expect(errorMessage).toBe('');
   });
   test('When a user wins the game, a message is displayed', async () => {
-    await page.click('#cell-0-0');
-    await page.click('#cell-1-0');
-    await page.click('#cell-0-1');
-    await page.click('#cell-1-1');
-    await page.click('#cell-0-2');
+    await winTheGame();
     const message = await page.$eval('#message', el => el.textContent);
     expect(message).toBe('X wins!');
   });
   test('When a user wins the game, nothing happens when the user clicks on an empty cell', async () => {
-    await page.click('#cell-0-0');
-    await page.click('#cell-1-0');
-    await page.click('#cell-0-1');
-    await page.click('#cell-1-1');
-    await page.click('#cell-0-2');
+    await winTheGame();
     await page.click('#cell-1-2');
     expect(await page.$eval('#cell-1-2', el => el.textContent)).toBe('');
   });
-  test('When a user wins the game, nothing happens when the user clicks on an empty cell', async () => {
-    await page.click('#cell-0-0');
-    await page.click('#cell-1-0');
-    await page.click('#cell-0-1');
-    await page.click('#cell-1-1');
-    await page.click('#cell-0-2');
+  test('When a user wins the game, nothing happens when the user clicks on an occupied cell', async () => {
+    await winTheGame();
     await page.click('#cell-0-0');
     const message = await page.$eval('#message', el => el.textContent);
     expect(message).toBe('X wins!');
   });
   test('when there is a draw, a message is displayed', async () => {
-    await page.click('#cell-0-0');
-    await page.click('#cell-0-1');
-    await page.click('#cell-0-2');
-    await page.click('#cell-1-0');
-    await page.click('#cell-1-2');
-    await page.click('#cell-1-1');
-    await page.click('#cell-2-0');
-    await page.click('#cell-2-2');
-    await page.click('#cell-2-1');
+    await drawTheGame();
     const message = await page.$eval('#message', el => el.textContent);
     expect(message).toBe('Draw!');
   });
   test('when there is a draw, nothing happens when the user clicks on a cell', async () => {
-    await page.click('#cell-0-0');
-    await page.click('#cell-0-1');
-    await page.click('#cell-0-2');
-    await page.click('#cell-1-0');
-    await page.click('#cell-1-2');
-    await page.click('#cell-1-1');
-    await page.click('#cell-2-0');
-    await page.click('#cell-2-2');
-    await page.click('#cell-2-1');
+    await drawTheGame();
     await page.click('#cell-1-1');
     const message = await page.$eval('#message', el => el.textContent);
     expect(message).toBe('Draw!');
